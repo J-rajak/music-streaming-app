470acf4621ec0ba8a9d366e6eaa7696c9a03ee12
Jeron Rajak <rajak.jeron@gmail.com>
2024-03-17 11:45:15 +0545

    created some api for admin functionalities

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const Song = require("../models/Song");
+const Artiste = require("../models/Artiste");
+const Album = require("../models/Album");
 const asyncHandler = require("express-async-handler");
 const cloudinary = require("../config/cloudinary");
 
@@ -87,7 +90,7 @@ const uploadImage = asyncHandler(async (req, res) => {
   if (!publicId) {
     const result = await cloudinary.uploader.upload(req.file.path, {
       transformation: [{ quality: "auto", width: 200, height: 200 }],
-      folder: "echosync",
+      folder: "echosync/users",
     });
     await user.updateOne({ image: result.secure_url });
   } else {
@@ -95,7 +98,7 @@ const uploadImage = asyncHandler(async (req, res) => {
       public_id: publicId,
       overwrite: true,
       transformation: [{ quality: "auto", width: 200, height: 200 }],
-      folder: "echosync",
+      folder: "echosync/users",
     });
     await user.updateOne({ image: result.secure_url });
   }
@@ -123,19 +126,17 @@ const getUser = asyncHandler(async (req, res, next) => {
 });
 
 //update user
-const updateUser = asyncHandler(async(req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
-  if(!user) {
-    return res.status(400).json({message: "user not found"})
-
+  if (!user) {
+    return res.status(400).json({ message: "user not found" });
   }
   user.username = req.body.username || user.username;
   user.email = req.body.email || user.email;
   user.isAdmin = req.body.isAdmin;
   user.isPremium = req.body.isPremium;
-
-})
+});
 
 // Delete user
 const deleteUser = asyncHandler(async (req, res) => {
@@ -145,6 +146,105 @@ const deleteUser = asyncHandler(async (req, res) => {
     return res.status(404).json({ message: "User not found" });
   }
   res.status(200).json({ message: "user deleted successfully" });
+});
+
+//upload song
+const uploadSong = asyncHandler(async (req, res) => {
+  const { title, artiste, duration, genre, lyrics } = req.body;
+  const { coverImage, audioURL } = req.file;
+
+  const songUploadResponse = await cloudinary.uploader.upload(
+    audioURL[0].path,
+    {
+      folder: "echosync/songs",
+      resource_type: "mp3",
+    }
+  );
+
+  const imageUploadResponse = await cloudinary.uploader.upload(
+    coverImage[0].path,
+    {
+      transformation: [{ quality: "auto", width: 200, height: 200 }],
+      folder: "echosync/songs/coverImage",
+    }
+  );
+
+  const newSong = new Song({
+    title,
+    artiste,
+    duration,
+    releaseDate,
+    genre,
+    lyrics,
+    audioURL: songUploadResponse.secure_url,
+    coverImage: imageUploadResponse.secure_url,
+  });
+
+  await newSong.save();
+  if (!newSong) {
+    res.status(500).json({ message: "Failed to upload song" });
+  }
+
+  res.status(201).json(newSong);
+});
+
+// post artiste
+const postArtiste = asyncHandler(async (req, res) => {
+  const { name, bio } = req.body;
+
+  const uploadImage = await cloudinary.uploader.upload(req.file.path, {
+    transformation: [{ quality: "auto", width: 200, height: 200 }],
+    folder: "echosync/artiste",
+  });
+
+  const newArtiste = new Artiste({
+    name,
+    bio,
+    image: uploadImage.secure_url,
+  });
+
+  await newArtiste.save();
+  if (!newArtiste) {
+    res.status(500).json({ message: "Failed to create artiste" });
+  }
+  res.status(201).json(newArtiste);
+});
+
+// upload album
+const postAlbum = asyncHandler(async (req, res) => {
+  const { title, artiste, releaseDate, genre, songs } = req.body;
+
+  const foundArtiste = await Artiste.findOne({ name: artiste });
+
+  if (!foundArtiste) {
+    res.status(404).json({ message: "User not found" });
+  }
+
+  const foundSongs = await Song.findOne({ title: songs });
+
+  if (!foundSongs) {
+    res.status(404).json({ message: "song not found" });
+  }
+
+  const uploadImage = await cloudinary.uploader.upload(req.file.path, {
+    transformation: [{ quality: "auto", height: 200, width: 200 }],
+    folder: "echosync/albums",
+  });
+
+  const newAlbum = new Album({
+    title,
+    releaseDate,
+    genre,
+    coverImage: uploadImage.secure_url,
+    artiste: foundArtiste._id,
+    songs: foundSongs._id,
+  });
+
+  await newAlbum.save();
+  if (!newAlbum) {
+    res.status(404).json({ message: "album not appended" });
+  }
+  res.status(201).json(newAlbum);
 });
 
 module.exports = {
